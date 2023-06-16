def3ba376535dc8970509eda93ad95fcf849edf9
Rafatulislam60 <rafatulislam67@gmail.com>
2023-06-17 01:31:36 +0600

    add admin

--- index.js
+++ index.js
@@ -35,6 +35,11 @@ async function run() {
     const usersCollection = client.db("fluencyDb").collection("users");
 
     // users api
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    });
+
     app.put("/users/:email", async (req, res) => {
       const email = req.params.email;
       const user = req.body;
@@ -45,6 +50,19 @@ async function run() {
       };
       const result = await usersCollection.updateOne(query, updateDoc, options);
       console.log(result);
+      res.send(result);
+    });
+
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
       res.send(result);
     });
 
