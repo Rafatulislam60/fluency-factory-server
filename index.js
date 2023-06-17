9633d0a8877272771b207e11389d509df3fb74b8
Rafatulislam60 <rafatulislam67@gmail.com>
2023-06-17 14:31:15 +0600

    ready to submit

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// const verifyJWT = (req, res, next) => {
+//   const authorization = req.headers.authorization;
+//   if (!authorization) {
+//     return res
+//       .status(401)
+//       .send({ error: true, message: "unauthorized access" });
+//   }
+
+//   // bearer token
+//   const token = authorization.split("")[1];
+//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+//     if (err) {
+//       return res
+//         .status(401)
+//         .send({ error: true, message: "unauthorized access" });
+//     }
+//     req.decoded = decoded;
+//     next();
+//   });
+// };
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v7y5nyp.mongodb.net/?retryWrites=true&w=majority`;
@@ -34,6 +55,15 @@ async function run() {
       .db("fluencyDb")
       .collection("instructors");
     const usersCollection = client.db("fluencyDb").collection("users");
+
+    // // jwt token
+    // app.post("/jwt", (req, res) => {
+    //   const user = req.body;
+    //   const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    //     expiresIn: "1hr",
+    //   });
+    //   res.send({ token });
+    // });
 
     // users api
     app.get("/users", async (req, res) => {
@@ -67,7 +97,7 @@ async function run() {
       res.send(result);
     });
 
-    // verify admin
+    // verify admin verifyJWT,
     app.get("/users/admin/:email", async (req, res) => {
       const email = req.params.email;
       const decodedEmail = req.decoded.email;
@@ -78,17 +108,19 @@ async function run() {
       const query = { email: email };
       const user = await usersCollection.findOne(query);
 
-      const result = { admin: user?.role === "admin" };
-      res.send(result);
+      if (user) {
+        const result = { admin: user?.role === "admin" };
+        res.send(result);
+      }
     });
 
-    // verify instructor
-    app.get("/users/instructor/:email", async (req, res) => {
-      const email = req.params.email;
-      const decodedEmail = req.decoded.email;
-      if (email !== decodedEmail) {
-        res.send({ instructor: false });
-      }
+    // // verify instructor
+    // app.get("/users/instructor/:email", async (req, res) => {
+    //   const email = req.params.email;
+    //   const decodedEmail = req.decoded.email;
+    //   if (email !== decodedEmail) {
+    //     res.send({ instructor: false });
+    //   }
 
       const query = { email: email };
       const user = await usersCollection.findOne(query);
